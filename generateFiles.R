db31c69fd9ceccf3bcc1a28e27e03472ef9ad9b6
fauberso <fauberso@csc.com>
2017-03-06 09:57:21 +0100

    Corrected behaviour on incremental compute

--- generateFiles.R
+++ generateFiles.R
@@ -1,6 +1,7 @@
 library(Matrix)
 library(quanteda)
 library(readtext)
+library(parallel)
 
 #install readtext using devtools::install_github("kbenoit/readtext")
 #install quanteda using devtools::install_github("kbenoit/quanteda")
@@ -10,11 +11,19 @@ library(readtext)
 k <- 5 #"good enough" value above which we don't discount anymore
 discProp <- "$discounts" #Name of the Discount table in an environment
 
+# 
+# Print a debug message
+#
+debugLog <- function(...) {
+  cat(paste(Sys.time(), paste(...), sep=" - "))
+  cat("\n")
+}
+
 #
 # Make an n-percent sample
 #
 makeSample <- function(originalDir, sampledDir, filename, samplePercent) {
-  print(paste("Sampling ", filename, "...", sep = ""))
+  debugLog(paste("Sampling ", filename, "...", sep = ""))
   setwd(originalDir)
   connection <- file(description=filename, open="rb")
   contents <- readLines(connection, skipNul = TRUE, encoding = "UTF-8")
@@ -28,8 +37,8 @@ makeSample <- function(originalDir, sampledDir, filename, samplePercent) {
 # Make a DFM of N-Grams
 #
 getDFM <- function(c, ...) {
-  print("Generating NGram...")
-  dfm(c, stem=FALSE, tolower=TRUE, removeNumbers=TRUE, removePunct=TRUE, removeSymbols=TRUE, removeHyphens=FALSE, removeTwitter=TRUE, removeURL=TRUE, ...)
+  debugLog("Generating NGram...")
+  dfm(c, stem=FALSE, tolower=TRUE, removeNumbers=TRUE, removePunct=TRUE, removeSymbols=TRUE, removeHyphens=TRUE, removeTwitter=TRUE, removeURL=TRUE, ...)
 }
 
 #
@@ -37,16 +46,20 @@ getDFM <- function(c, ...) {
 #
 #Usage example: get('and_the_facts', envir=predictor, inherits = FALSE)
 #Check whether an ngram-1 exists with: exists('and_the_fact', envir=predictor, inherits = FALSE)
+#
 toPredictorMap <- function(dfm) {
-  #Use an R environment: Effectively a native Hash map implementation 
-  predictor <- new.env(hash = TRUE, parent=emptyenv())
-  print("Summing ngram frequencies...")
+  #Variant 1: Use an R environment: Effectively a native Hash map implementation 
+  predictor <- new.env(hash = TRUE, parent=emptyenv(), size = 5000000)
+  #Variant 2: Use a list (More readable, but much slower for large amounts of data)
+  #predictor <- list()
+    
+  debugLog("Summing ngram frequencies...")
   frequencies <- colSums(dfm_sort(dfm, decreasing = FALSE))
   rm(dfm)
   
   emptylist <- list(c())
   # Preparations for Good-Turing estimator
-  print("Calculating discounts...")
+  debugLog("Calculating discounts...")
   fofTable <- table(frequencies)
   discounts <- sapply(1:k, function(i) ( ((i+1)/i) * (fofTable[names(fofTable)==i+1]/fofTable[names(fofTable)==i]) ) )
   discounts <- unname(discounts)
@@ -61,7 +74,7 @@ toPredictorMap <- function(dfm) {
   }
   
   #Iterate through ngrams in increasing order of frequency
-  print("Splitting NGrams and collating frequencies of ngrams with identical root...")
+  debugLog("Splitting NGrams and collating frequencies of ngrams with identical root...")
   pb <- txtProgressBar(min = 1, max = length(frequencies), style = 3)
   
   for (i in 1:length(frequencies)) {
@@ -76,42 +89,104 @@ toPredictorMap <- function(dfm) {
     #The sorting means that the more frequent occurences will overwrite less frequents. The frequencies
     #of the previous occurences will be kept (in descending order) so that we're later able to compute
     #a good-turing estimator.
+    # Variant 1
     entry <- mget(previous, envir=predictor, inherits = FALSE, ifnotfound = emptylist)[[1]]
     assign(previous,  c(nextfreq, entry), pos=predictor)
+    # Variant 2
+    # predictor[[previous]] <- c(nextfreq, predictor[[previous]])
     
     setTxtProgressBar(pb, i)
   }
   
-  return(predictor)
-  
-  # Each key is now an n-gram, each element is a list, consisting of a predicted value and a list of 
-  # frequencies. For each element, replace the list of frequencies with a good-turing estimation of the
-  # combined proportion of undetected species. This estimation dictates in the back-off algorithm whether
-  # we prefer using the result from a lower-order ngram instead, beacuse we suspect the followin word
-  # is most likely to be one we haven't encountered.
-  print("Calculating Good-Turing estimation of combined proportion of undetected species...")
-  k <- 5 #"good enough" value above which we don't discount anymore
-  fofTable <- table(frequencies)
-  discMultiplier <- sapply(1:k, function(i) (fofTable[names(fofTable)==i+1]/fofTable[names(fofTable)==i]) )
-  
-  predictor <- list2env(eapply(predictor, function(e) {
-    results <- data.frame(freq=e[[2]])
-    rownames(results) <- e[[1]]
-    
-    results$discounts <- sapply(results$freq, function(freq) {
-      if(freq>k) {
-        1
-      } else {
-        ((freq+1)/freq) * discMultiplier[freq]
-      }
-    }
-    )
-    
-    results
-  }), hash = TRUE, parent=emptyenv())
-  
-  predictor #A hash map, with an ngram-1 as key, and a data frame with (next word, freq, gt discount) as value 
+  # Variant 1
+  as.list.environment(predictor)
+  # Variant 2
+  # predictor
 }
+
+#
+# Given a list of frequencies for classes, and a list of Good-Turing discounts (in which the first element is the
+# discount for single occurences, the second is the discount for 2 occurences, etc..), calculate the 
+# probability that an item is not in any of the classes, but rather from a missing, unknown class.
+# 
+getResidualProb <- function(flist, dlist, k) {
+  totalFreq<-sum(flist)
+  probabilities <- ifelse(flist > k, flist, dlist[flist]*flist)
+  1-sum(probabilities/totalFreq)
+}
+
+
+computeResidualProb <- function(predictorMap) {
+  cl <- makeCluster(8)
+  clusterExport(cl=cl, varlist=c("getResidualProb", "k"))  
+  dlist <- predictorMap[[discProp]] #List of good-turing discounts
+  residuals <- parLapply(cl, predictorMap, function(x) getResidualProb(x, dlist, k))
+  stopCluster(cl)
+  
+  residuals
+}
+
+#
+# In an underscore-separated ngram, remove the first word and underscore
+#
+removeFirstTerm <- function(term) {
+  substr(term, regexpr("_", term)+1, nchar(term))
+}
+
+#
+# Calculate probability for each prediction for an n-gram. Check in corresponding n-1gram whether the same term
+# exists. If yes, and if its backed off probability is higher than its probability in the n-gram, then remove
+# it: During prediction, we're better off using the backed-off model, since it yields a higher probability
+# for the same predicted term.
+# Return the entire model, but using probabilities instead of frequencies.
+#
+filterModel <- function(model, backoff) {
+  cl <- makeCluster(8)
+  clusterExport(cl=cl, varlist=c("removeFirstTerm", "k", "discProp")) 
+  
+  modelDisc <- model[[discProp]]
+  backoffDisc <- backoff[[discProp]]
+
+  #results <- sapply(1:length(model), function(i) {
+  results <- parSapply(cl, 1:length(model), function(i) {
+
+    values <- model[[i]]
+    name <- names(model)[i]
+
+    if(name==discProp) {
+      retval <- list()
+      retval[[name]]<-values
+      return(retval)
+    }
+    
+    backoffValues <- backoff[removeFirstTerm(name)][[1]]
+
+    #Remove from the n-1gram any values we already have as part of the ngram
+    # backoffValues <- backoffValues[!(names(backoffValues) %in% names(values))]
+    
+    # Convert the frequencies to probabilities
+    values <- ifelse(values > k, values, modelDisc[values]*values)/sum(values)
+    backoffValues <- ifelse(backoffValues > k, backoffValues, backoffDisc[backoffValues]*backoffValues)/sum(backoffValues)
+    backoffValues <- backoffValues * (1-sum(values))
+    
+    # If probability from a value is lower than the backed off probability of the same value,
+    # then there's no point in keeping the value. We will used the backed off probability in the predictor anyway
+    filter <- Vectorize(function(x, name) is.na(backoffValues[name]) || backoffValues[name]<x)
+    values <- values[filter(values, names(values))]
+    
+    # Lastly, remove all values below a certain threshold. This will prevent us from keeping very long list with
+    # values that are not very significant
+    values <- values[values>0.001]
+    
+    retval <- list()
+    retval[[name]]<-values
+    retval
+  })
+  
+  stopCluster(cl)
+  results[!sapply(results, function(x) is.null(x) || length(x)==0)] 
+}
+
 
 # Main block:
 # Download, Sample, make ngrams, save.
@@ -121,9 +196,10 @@ computeFiles <- function(samplePercent=1, parallel=TRUE) {
   finalDirName <- "final"
   sampledDirName <- paste("sample",samplePercent, sep=".")
   sampledDir <- paste(getwd(),sampledDirName,sep="/")
-  dataDirName <- paste("data",samplePercent, sep=".")
+  dataDirName <- paste("model",samplePercent, sep=".")
   dataDir <- paste(getwd(),dataDirName,sep="/")
-  print(paste("Using sample under:", sampledDirName))
+  debugLog(paste("Using sample under:", sampledDirName))
+  modelFileName <- paste("model",samplePercent, "rds", sep=".")
   
   # Move one directory down if we're still in our project dir
   if (file.exists(projectDirName)){
@@ -150,7 +226,7 @@ computeFiles <- function(samplePercent=1, parallel=TRUE) {
   }
   
   # Compute simple data files
-  if (TRUE & !file.exists(dataDir)){
+  if (!file.exists(paste(dataDir, "predictor.1.rds", sep="/"))){
     setwd(sampledDir)
     texts <- readtext("final/en_US/*.txt", docvarsfrom="filenames", dvsep="\\.", docvarnames=c("lang","name"), encoding="latin1")
     c <- corpus(texts, textField = "text")
@@ -160,19 +236,54 @@ computeFiles <- function(samplePercent=1, parallel=TRUE) {
     summary(c)
     dir.create(dataDir, recursive = TRUE)
 
-    print("Processing 2-grams into 1-gram and a predicted value...")
-    saveRDS(toPredictorMap(getDFM(c, ngrams=2)), file=paste(dataDir, "unigrams.rds", sep="/"))
-    print("Processing 3-grams into 2-gram and a predicted value...")
-    saveRDS(toPredictorMap(getDFM(c, ngrams=3)), file=paste(dataDir, "bigrams.rds", sep="/"))
-    print("Processing 4-grams into 3-gram and a predicted value...")
-    saveRDS(toPredictorMap(getDFM(c, ngrams=4)), file=paste(dataDir, "trigrams.rds", sep="/"))
+    debugLog("Processing 2-grams into 1-gram and a predicted value...")
+    saveRDS(toPredictorMap(getDFM(c, ngrams=2)), file=paste(dataDir, "predictor.1.rds", sep="/"))
+    debugLog("Processing 3-grams into 2-gram and a predicted value...")
+    saveRDS(toPredictorMap(getDFM(c, ngrams=3)), file=paste(dataDir, "predictor.2.rds", sep="/"))
+    debugLog("Processing 4-grams into 3-gram and a predicted value...")
+    saveRDS(toPredictorMap(getDFM(c, ngrams=4)), file=paste(dataDir, "predictor.3.rds", sep="/"))
+    rm(c)
   }
-    
-  dataDirName
+  
+  # Compute rediduals (currently unused)
+  if (!file.exists(paste(dataDir, "residual.1.rds", sep="/"))){
+    debugLog("Calculating residuals of 1-gram predictor...")
+    saveRDS(computeResidualProb(readRDS(file=paste(dataDir, "predictor.1.rds", sep="/"))), 
+                                file=paste(dataDir, "residual.1.rds", sep="/"))
+    debugLog("Calculating residuals of 2-gram predictor...")
+    saveRDS(computeResidualProb(readRDS(file=paste(dataDir, "predictor.2.rds", sep="/"))), 
+            file=paste(dataDir, "residual.2.rds", sep="/"))
+    debugLog("Calculating residuals of 3-gram predictor...")
+    saveRDS(computeResidualProb(readRDS(file=paste(dataDir, "predictor.3.rds", sep="/"))), 
+            file=paste(dataDir, "residual.3.rds", sep="/"))
+  }
+  
+  # Filter results and combine into 1 file
+  if (!file.exists(modelFileName)){
+    print(ls())
+    model <- list()
+    debugLog("Removing 3-grams less significant than their backed off 2-grams...")
+    pred3 <- readRDS(file=paste(dataDir, "predictor.3.rds", sep="/"))
+    pred2 <- readRDS(file=paste(dataDir, "predictor.2.rds", sep="/"))
+    model[[3]] <- filterModel(pred3,pred2)
+    debugLog("Removing 3-grams less significant than their backed off 2-grams...")
+    rm(pred3)
+    pred1 <- readRDS(file=paste(dataDir, "predictor.1.rds", sep="/"))
+    model[[2]] <- filterModel(pred2,pred1)
+    debugLog("Removing 1-grams with low significance...")
+    rm(pred2)
+    model[[1]] <- filterModel(pred1, list());
+    rm(pred1)
+    saveRDS(model, file=modelFileName)
+  }
+  
+  TRUE
 }
 
 batchCompute <- function() {
   computeFiles(1)
+  computeFiles(2)
+  computeFiles(5)
   computeFiles(10)
   computeFiles(20)
 }
--- predict.R
+++ predict.R
@@ -2,9 +2,19 @@
 k <- 5 #"good enough" value above which we don't discount anymore
 discProp <- "$discounts" #Name of the Discount table in an environment
 
+# 
+# Print a debug message
+#
+debugLog <- function(...) {
+  cat(paste(Sys.time(), paste(...), sep=" - "))
+  cat("\n")
+}
+
 #
 # Load data files into the environment 
+# Measure load times with: system.time(load())
 #
+
 load <- function(samplePercent=1) {
   projectDirName <- "CourseraDataScienceCapstone"
   
@@ -14,24 +24,67 @@ load <- function(samplePercent=1) {
   }
   originalDir <- getwd()
   
-  dataDirName <- paste("data",samplePercent, sep=".")
+  dataDirName <- paste("model",samplePercent, sep=".")
   dataDir <- paste(getwd(),dataDirName,sep="/")
   
-  assign("unigrams", readRDS(file=paste(dataDir, "unigrams.rds", sep="/")), pos=globalenv())
-  assign("bigrams", readRDS(file=paste(dataDir, "bigrams.rds", sep="/")), pos=globalenv())
-  assign("trigrams", readRDS(file=paste(dataDir, "trigrams.rds", sep="/")), pos=globalenv())
+  start.time <- Sys.time()
+  debugLog("Loading from ",dataDir, "...")
+  assign("pred1", convert(readRDS(file=paste(dataDir, "predictor.1.rds", sep="/")), parent=emptyenv(), size = 15000000), pos=globalenv())
+  debugLog("Predictor 1 loaded")
+  assign("pred2", convert(readRDS(file=paste(dataDir, "predictor.2.rds", sep="/")), parent=emptyenv(), size = 50000000), pos=globalenv())
+  debugLog("Predictor 2 loaded")
+  assign("pred3", convert(readRDS(file=paste(dataDir, "predictor.3.rds", sep="/")), parent=emptyenv(), size = 100000000), pos=globalenv())
+  debugLog("Predictor 3 loaded")
+  assign("resi1", convert(readRDS(file=paste(dataDir, "residual.1.rds", sep="/")), parent=emptyenv(), size = 15000000), pos=globalenv())
+  debugLog("Residuals from Predictor 1 loaded")
+  assign("resi2", convert(readRDS(file=paste(dataDir, "residual.2.rds", sep="/")), parent=emptyenv(), size = 50000000), pos=globalenv())
+  debugLog("Residuals from Predictor 2 loaded")
+  assign("resi3", convert(readRDS(file=paste(dataDir, "residual.3.rds", sep="/")), parent=emptyenv(), size = 100000000), pos=globalenv())
+  debugLog("Residuals from Predictor 3 loaded")
+  end.time <- Sys.time()
+  
+  debugLog("Load complete in",end.time - start.time,"seconds")
+}
+
+convert <- function(data, ...) {
+  #list2env(data, ...)
+  data
+}
+
+getSearchTerm <- function(words, count) {
+  if (length(words)-(count-1)<1) return(NULL)
+  paste(words[(length(words)-(count-1)):length(words)], collapse="_")
+}
+
+getPrediction <- function(searchTerm, pred) {
+  if (is.null(searchTerm)) return (c())
+  discounts <- pred[[discProp]]
+  frequencies <- pred[[searchTerm]]
+  total <- sum(frequencies) 
+  ifelse(frequencies > k, frequencies, discounts[frequencies]*frequencies)/total
 }
 
 predict <- function(string, searchTerms=NULL) {
   words <- strsplit(string, " ")[[1]]
-  getSearchTerm <- function(words, count) paste(words[(length(words)-(count-1)):length(words)], collapse="_")
-  
+
   emptylist <- list(c())
   
-  resultsFromTrigrams <- mget(getSearchTerm(words,3), envir=trigrams, inherits = FALSE, ifnotfound = emptylist)[[1]]
-  resultsFromBigrams <- mget(getSearchTerm(words,2), envir=bigrams, inherits = FALSE, ifnotfound = emptylist)[[1]]
-  resultsFromUnigrams <- mget(getSearchTerm(words,1), envir=unigrams, inherits = FALSE, ifnotfound = emptylist)[[1]]
-
+  #resultsFromTrigrams <- mget(getSearchTerm(words,3), envir=trigrams, inherits = FALSE, ifnotfound = emptylist)[[1]]
+  resultsFromTrigrams  <- getPrediction(getSearchTerm(words,3), pred3)
+  redidualFromTrigrams <- 1-sum(resultsFromTrigrams)
+  
+  #resultsFromBigrams <- mget(getSearchTerm(words,2), envir=bigrams, inherits = FALSE, ifnotfound = emptylist)[[1]]
+  resultsFromBigrams   <- getPrediction(getSearchTerm(words,2), pred2)
+  redidualFromBigrams  <- 1-sum(resultsFromBigrams)
+  resultsFromBigrams   <- resultsFromBigrams * redidualFromTrigrams
+  
+  #resultsFromUnigrams <- mget(getSearchTerm(words,1), envir=unigrams, inherits = FALSE, ifnotfound = emptylist)[[1]]
+  resultsFromUnigrams  <- getPrediction(getSearchTerm(words,1), pred1)
+  redidualFromUnigrams <- 1-sum(resultsFromUnigrams)
+  resultsFromUnigrams  <- resultsFromUnigrams * redidualFromBigrams
+  
+  debugLog("Residual probabilities:", redidualFromTrigrams, "from Trigrams,", redidualFromBigrams, "from Bigrams,", redidualFromUnigrams, "probability of non match.")
+  
   results <- c(resultsFromTrigrams, resultsFromBigrams, resultsFromUnigrams)
   
   if (!is.null(searchTerms)) {
